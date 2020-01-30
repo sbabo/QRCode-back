75730ad4fcef4f69417a146fb6f0ea797989e8b0
Samuel <babo.samuel0@gmail.com>
2020-01-30 14:57:58 +0100

    API documentation swagger

--- src/main/java/com/qrcode/qrcode/QrcodeApplication.java
+++ src/main/java/com/qrcode/qrcode/QrcodeApplication.java
@@ -2,13 +2,17 @@ package com.qrcode.qrcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
+@EnableSwagger2WebMvc
+@Import( SpringDataRestConfiguration.class )
 public class QrcodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QrcodeApplication.class, args);
-		System.out.println("C'est parti !");
 	}
 
 }
--- src/main/java/com/qrcode/qrcode/controllers/CodeController.java
+++ src/main/java/com/qrcode/qrcode/controllers/CodeController.java
@@ -4,10 +4,32 @@ import com.qrcode.qrcode.Exceptions.CodeNotFoundException;
 import com.qrcode.qrcode.bo.Code;
 import com.qrcode.qrcode.repository.CodeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 import java.awt.print.Book;
 import java.util.List;
 
+@Validated
+@RestController
+@RequestMapping("/codes")
+public class CodeController {
+
+    private CodeRepository repository;
+    @GetMapping
+    public ResponseEntity<List<Code>> findAll(
+            @Valid @Positive @RequestParam(required = false, defaultValue = "1") int page,
+            @Valid @Positive @RequestParam(required = false, defaultValue = "10") int size) {
+        HttpHeaders headers = new HttpHeaders();
+        Page<Code> codes = repository.findAll(PageRequest.of(page, size));
+        headers.add("X-Users-Total", Long.toString(codes.getTotalElements()));
+        return new ResponseEntity<>(codes.getContent(), headers, HttpStatus.OK);
+    }
+}
